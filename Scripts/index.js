81afc39e150e16dd8018e1254e7b28e8952514ca
damianmercado32@gmail.com <61065466+rocky-recon@users.noreply.github.com>
2021-07-04 09:31:03 -0700

    form works

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,33 +1,33 @@
-$(document).ready(() => {
-  $(".nav-item").on("click", (e) => {
-    if ($(".active").length) {
-      $($(".active")[0]).removeClass("active");
-    }
+// $(document).ready(() => {
+//   $(".nav-item").on("click", (e) => {
+//     if ($(".active").length) {
+//       $($(".active")[0]).removeClass("active");
+//     }
 
-    $($(e.target).parent()[0]).addClass("active");
-  });
+//     $($(e.target).parent()[0]).addClass("active");
+//   });
 
-document
-  .getElementById("contacts")
-  .addEventListener("click", handleSubmitContactForm);
+// document
+//   .getElementById("contacts")
+//   .addEventListener("click", handleSubmitContactForm);
 
-function handleSubmitContactForm(event) {}
+// function handleSubmitContactForm(event) {}
 
-posFilterBar($(".filter").first());
+// posFilterBar($(".filter").first());
 
-$(".filter").click(function () {
-  posFilterBar(this);
-});
+// $(".filter").click(function () {
+//   posFilterBar(this);
+// });
 
-function posFilterBar(elem) {
-  var origin = $(elem).parent().offset().left;
-  var pos = $(elem).offset().left;
-  $(".float-bar").css({
-    left: pos - origin,
-    width: $(elem).innerWidth(),
-  });
-  $(".float-bar .row").css("left", (pos - origin) * -1);
-}
+// function posFilterBar(elem) {
+//   var origin = $(elem).parent().offset().left;
+//   var pos = $(elem).offset().left;
+//   $(".float-bar").css({
+//     left: pos - origin,
+//     width: $(elem).innerWidth(),
+//   });
+//   $(".float-bar .row").css("left", (pos - origin) * -1);
+// }
 
 // // GALLERY
 // $("#gallery").mixItUp({});
@@ -38,26 +38,60 @@ function posFilterBar(elem) {
 //   }, 2000);
 // }
 
-// CONTACT FORM
-$('#contact-form').submit(function(e) {
-  e.preventDefault();
+// // CONTACT FORM
+// $('#contact-form').submit(function(e) {
+//   e.preventDefault();
 
-  $.ajax({
-      url: 'https://formspree.io/mattwilliams85@gmail.com',
-      method: 'POST',
-      data: { message: $('form').serialize() },
-      dataType: 'json'
-    }).done(function(response) {
-      $('#success').addClass('expand');
-      $('#contact-form')
-        .find('input[type=text], input[type=email], textarea')
-        .val('');
-    });
+//   $.ajax({
+//       url: 'https://formspree.io/mattwilliams85@gmail.com',
+//       method: 'POST',
+//       data: { message: $('form').serialize() },
+//       dataType: 'json'
+//     }).done(function(response) {
+//       $('#success').addClass('expand');
+//       $('#contact-form')
+//         .find('input[type=text], input[type=email], textarea')
+//         .val('');
+//     });
+//   });
+
+//   $('#close').click(function() {
+//     $('#success').removeClass('expand');
+//   });
+
+// }
+
+
+  /* paste this line in verbatim */
+  window.formbutton=window.formbutton||function(){(formbutton.q=formbutton.q||[]).push(arguments)};
+
+  /* customize formbutton below*/     
+  formbutton("create", {
+    action: "https://formspree.io/f/xoqylyrv",
+    title: "How can we help?",
+    fields: [
+      { 
+        type: "email", 
+        label: "Email:", 
+        name: "email",
+        required: true,
+        placeholder: "your@email.com"
+      },
+      {
+        type: "textarea",
+        label: "Message:",
+        name: "message",
+        placeholder: "What's on your mind?",
+      },
+      { type: "submit" }      
+    ],
+    styles: {
+      title: {
+        backgroundColor: "gray"
+      },
+      button: {
+        backgroundColor: "gray"
+      }
+    }
   });
-
-  $('#close').click(function() {
-    $('#success').removeClass('expand');
-  });
-
-}
-
+</script>
