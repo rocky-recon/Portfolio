5a355d61cfdef7aa4941f089769bf78dbd543226
damianmercado32@gmail.com <61065466+rocky-recon@users.noreply.github.com>
2021-07-21 18:44:04 -0700

    updated hover needs to be clicked

--- scripts1/index.js
+++ scripts1/index.js
@@ -1,65 +1,42 @@
-// $(document).ready(() => {
-//   $(".nav-item").on("click", (e) => {
-//     if ($(".active").length) {
-//       $($(".active")[0]).removeClass("active");
-//     }
+var $cards = $(".card-object"),
+  $faceButtons = $(".face");
 
-//     $($(e.target).parent()[0]).addClass("active");
-//   });
+$faceButtons.on("click", flipCard);
 
-// document
-//   .getElementById("contacts")
-//   .addEventListener("click", handleSubmitContactForm);
+function flipCard(event) {
+  event.preventDefault();
 
-// function handleSubmitContactForm(event) {}
+  var $btnFace = $(this),
+    $card = $btnFace.parent(".card-object");
 
-// posFilterBar($(".filter").first());
+  if ($card.hasClass("flip-in")) {
+    closeCards();
+  } else {
+    closeCards();
+    openCard($card);
+  }
+}
 
-// $(".filter").click(function () {
-//   posFilterBar(this);
-// });
+function closeCards() {
+  $cards.each(function () {
+    $(this)
+      .filter(".flip-in")
+      .removeClass("flip-in")
+      .queue(function () {
+        // Force reflow hack
+        var reflow = this.offsetHeight;
+        $(this).addClass("flip-out").dequeue();
+      });
+  });
+}
 
-// function posFilterBar(elem) {
-//   var origin = $(elem).parent().offset().left;
-//   var pos = $(elem).offset().left;
-//   $(".float-bar").css({
-//     left: pos - origin,
-//     width: $(elem).innerWidth(),
-//   });
-//   $(".float-bar .row").css("left", (pos - origin) * -1);
-// }
-
-// // GALLERY
-// $("#gallery").mixItUp({});
-
-// function mixClear() {
-//   setTimeout(function () {
-//     $("#gallery").removeClass("waypoint");
-//   }, 2000);
-// }
-
-// // CONTACT FORM
-// $('#contact-form').submit(function(e) {
-//   e.preventDefault();
-
-//   $.ajax({
-//       url: 'https://formspree.io/mattwilliams85@gmail.com',
-//       method: 'POST',
-//       data: { message: $('form').serialize() },
-//       dataType: 'json'
-//     }).done(function(response) {
-//       $('#success').addClass('expand');
-//       $('#contact-form')
-//         .find('input[type=text], input[type=email], textarea')
-//         .val('');
-//     });
-//   });
-
-//   $('#close').click(function() {
-//     $('#success').removeClass('expand');
-//   });
-
-// }
+function openCard($card) {
+  $card.removeClass("flip-out").queue(function () {
+    // Force reflow hack
+    var reflow = this.offsetHeight;
+    $(this).addClass("flip-in").dequeue();
+  });
+}
 
 /* paste this line in verbatim */
 window.formbutton =
