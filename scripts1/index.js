64d341e4c36627b2cec8e51da7c8d179ed8df7aa
damianmercado32@gmail.com <61065466+rocky-recon@users.noreply.github.com>
2021-07-22 15:54:37 -0700

    updated bull gates image

--- scripts1/index.js
+++ scripts1/index.js
@@ -1,42 +1,42 @@
-var $cards = $(".card-object"),
-  $faceButtons = $(".face");
+// var $cards = $(".card-object"),
+//   $faceButtons = $(".face");
 
-$faceButtons.on("click", flipCard);
+// $faceButtons.on("click", flipCard);
 
-function flipCard(event) {
-  event.preventDefault();
+// function flipCard(event) {
+//   event.preventDefault();
 
-  var $btnFace = $(this),
-    $card = $btnFace.parent(".card-object");
+//   var $btnFace = $(this),
+//     $card = $btnFace.parent(".card-object");
 
-  if ($card.hasClass("flip-in")) {
-    closeCards();
-  } else {
-    closeCards();
-    openCard($card);
-  }
-}
+//   if ($card.hasClass("flip-in")) {
+//     closeCards();
+//   } else {
+//     closeCards();
+//     openCard($card);
+//   }
+// }
 
-function closeCards() {
-  $cards.each(function () {
-    $(this)
-      .filter(".flip-in")
-      .removeClass("flip-in")
-      .queue(function () {
-        // Force reflow hack
-        var reflow = this.offsetHeight;
-        $(this).addClass("flip-out").dequeue();
-      });
-  });
-}
+// function closeCards() {
+//   $cards.each(function () {
+//     $(this)
+//       .filter(".flip-in")
+//       .removeClass("flip-in")
+//       .queue(function () {
+//         // Force reflow hack
+//         var reflow = this.offsetHeight;
+//         $(this).addClass("flip-out").dequeue();
+//       });
+//   });
+// }
 
-function openCard($card) {
-  $card.removeClass("flip-out").queue(function () {
-    // Force reflow hack
-    var reflow = this.offsetHeight;
-    $(this).addClass("flip-in").dequeue();
-  });
-}
+// function openCard($card) {
+//   $card.removeClass("flip-out").queue(function () {
+//     // Force reflow hack
+//     var reflow = this.offsetHeight;
+//     $(this).addClass("flip-in").dequeue();
+//   });
+// }
 
 /* paste this line in verbatim */
 window.formbutton =
