806385206732223396521142ba6e2e245a2305b8
damianmercado32@gmail.com <61065466+rocky-recon@users.noreply.github.com>
2021-07-21 18:19:33 -0700

    update to format. Looks cleaner

--- scripts1/index.js
+++ scripts1/index.js
@@ -61,37 +61,39 @@
 
 // }
 
+/* paste this line in verbatim */
+window.formbutton =
+  window.formbutton ||
+  function () {
+    (formbutton.q = formbutton.q || []).push(arguments);
+  };
 
-  /* paste this line in verbatim */
-  window.formbutton=window.formbutton||function(){(formbutton.q=formbutton.q||[]).push(arguments)};
-
-  /* customize formbutton below*/     
-  formbutton("create", {
-    action: "https://formspree.io/f/xoqylyrv",
-    title: "How can we help?",
-    fields: [
-      { 
-        type: "email", 
-        label: "Email:", 
-        name: "email",
-        required: true,
-        placeholder: "your@email.com"
-      },
-      {
-        type: "textarea",
-        label: "Message:",
-        name: "message",
-        placeholder: "What's on your mind?",
-      },
-      { type: "submit" }      
-    ],
-    styles: {
-      title: {
-        backgroundColor: "gray"
-      },
-      button: {
-        backgroundColor: "gray"
-      }
-    }
-  });
-</script>
+/* customize formbutton below*/
+formbutton("create", {
+  action: "https://formspree.io/f/xoqylyrv",
+  title: "How can we help?",
+  fields: [
+    {
+      type: "email",
+      label: "Email:",
+      name: "email",
+      required: true,
+      placeholder: "your@email.com",
+    },
+    {
+      type: "textarea",
+      label: "Message:",
+      name: "message",
+      placeholder: "What's on your mind?",
+    },
+    { type: "submit" },
+  ],
+  styles: {
+    title: {
+      backgroundColor: "gray",
+    },
+    button: {
+      backgroundColor: "gray",
+    },
+  },
+});
